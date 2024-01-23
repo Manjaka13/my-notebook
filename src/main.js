140f57445c67d5609dfcbf3aefe5164ca1a24128
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-23 20:09:19 +0300

    setup base

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "@/styles/vars.css";
+import "@/styles/main.css";
+
+/**
+ * Entry point
+ */
 
 createApp(App).mount("#app");
