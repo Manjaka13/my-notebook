3eb710d12f11464da660e7448413c39ec8887327
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-24 00:44:41 +0300

    fix tasklist

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,19 +1,39 @@
 <template>
 	<ul class="tasklist">
-		<li class="tasklist__item">
-			<task-item />
+		<li class="tasklist__item" v-for="task in tasks" :key="task.id">
+			<task-item
+				:title="task.title"
+				:description="task.description"
+				:status="task.status"
+			/>
 		</li>
 	</ul>
 </template>
 
 <script>
-import "@/styles/tasklist.css";
 import TaskItem from "@/components/TaskItem.vue";
+import { getTasks, setDefaultTasks } from "@/helpers/utils";
+import { DEFAULT_TASKS } from "@/helpers/const";
+import "@/styles/tasklist.css";
 
 export default {
 	name: "TaskList",
 	components: {
 		TaskItem,
+	},
+	data() {
+		return {
+			tasks: getTasks(),
+		};
+	},
+	created() {
+		if (this.tasks.length === 0) {
+			setDefaultTasks();
+			this.tasks = DEFAULT_TASKS;
+		}
+	},
+	mounted() {
+		console.log(this.tasks);
 	},
 };
 </script>
