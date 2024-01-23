72264da6cca5ad376651b3309d41d656dc388e7f
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-23 22:16:51 +0300

    move component entry point

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<social-links />
+	<note-book />
+</template>
+
+<script>
+import SocialLinks from "@/components/SocialLinks.vue";
+import NoteBook from "@/components/NoteBook.vue";
+
+export default {
+	name: "App",
+	components: {
+		SocialLinks,
+		NoteBook,
+	},
+};
+</script>
+
+<style lang="css" scoped>
+#app {
+	/* display: flex; */
+}
+</style>
