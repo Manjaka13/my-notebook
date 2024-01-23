abe45f6dfe4e6eb0bead4af74b62c742ef239cd9
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-24 00:44:23 +0300

    add default list

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -19,3 +19,62 @@ export const SOCIAL = [
 		icon: ["fab", "linkedin"],
 	},
 ];
+
+export const DEFAULT_TASKS = [
+	{
+		id: "b4a5e194-9cc7-4d6a-af6a-d98627e01159",
+		title: "Organize the work day",
+		description:
+			"Start the day with a clear agenda and simple instructions. Following these steps can increase productivity at work.",
+		status: false,
+	},
+	{
+		id: "38701da1-97c1-4c58-ad42-56c180578602",
+		title: "Discuss daily",
+		description:
+			"In order to better coordinate the actions of employees during the day, it is essential to discuss the procedure to follow during a meeting in the morning.",
+		status: false,
+	},
+	{
+		id: "f9cf4b4a-35de-4839-968e-11ee65cf4995",
+		title: "Undertaking the first tasks",
+		description:
+			"Identify the most important and urgent tasks on the agenda and rank them according to what the agenda suggests using the Eisenhower Matrix.",
+		status: false,
+	},
+	{
+		id: "4407057d-0e71-404d-b116-8a85b0176242",
+		title: "10min coffee break",
+		description:
+			"It's important to stay focused on work, but you also need to take breaks to optimize your attention level and regain stamina for the rest of the day.",
+		status: false,
+	},
+	{
+		id: "3979fe39-b5d1-4d8a-9b97-8b9e97ab2635",
+		title: "Lunch",
+		description:
+			"Having a hearty meal but not too busy, a fairly balanced dish that will boost your energy. It's also a moment of relaxation with colleagues.",
+		status: false,
+	},
+	{
+		id: "13e8583a-c963-499d-ad65-f5b9cebc414a",
+		title: "Customer meeting about progress",
+		description:
+			"It is important that the client knows where their project is and if there are any particular blockages. You must know how to give a detailed report of your past and current work.",
+		status: false,
+	},
+	{
+		id: "83e27810-1025-4a8d-89f1-a1e5ea007059",
+		title: "Relaxation at the snack bar",
+		description:
+			"End the day on a good note, a little fun and relaxation. Share a friendly moment with colleagues to maintain social and human relations.",
+		status: false,
+	},
+	{
+		id: "88510c7a-460d-4417-a96e-c197437beddd",
+		title: "Come home!",
+		description:
+			"Come home and share moments with family and friends. Forget work for a bit and get busy with your personal hobbies.",
+		status: false,
+	},
+];
