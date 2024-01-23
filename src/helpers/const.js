6b0f122b65ae3cfd3e7464f9e7589144a8196b6b
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-23 22:16:02 +0300

    add constants file

---
+++ src/helpers/const.js
@@ -0,0 +1,21 @@
+/**
+ * Stores constants
+ */
+
+export const SOCIAL = [
+	{
+		title: "Reach me on Facebook !",
+		link: "#0",
+		icon: ["fab", "facebook"],
+	},
+	{
+		title: "View my repos",
+		link: "#0",
+		icon: ["fab", "github"],
+	},
+	{
+		title: "Contact me on LinkedIn",
+		link: "#0",
+		icon: ["fab", "linkedin"],
+	},
+];
