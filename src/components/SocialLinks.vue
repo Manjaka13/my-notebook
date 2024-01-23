67e53f6e9a1f6efc86b395b930536d1bdfd2cacf
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-23 22:16:59 +0300

    add social

---
+++ src/components/SocialLinks.vue
@@ -0,0 +1,23 @@
+<template>
+	<ul class="social-links">
+		<li class="social-links__item" v-for="(item, key) in social" :key="key">
+			<a className="link" :href="item.link" :title="item.title">
+				<font-icon :icon="item.icon" />
+			</a>
+		</li>
+	</ul>
+</template>
+
+<script>
+import { SOCIAL } from "@/helpers/const";
+import "@/styles/social-links.css";
+
+export default {
+	name: "SocialLinks",
+	data() {
+		return {
+			social: SOCIAL,
+		};
+	},
+};
+</script>
