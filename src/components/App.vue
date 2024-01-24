8869e4a25dc92e067dc7dcebd5e886ea1e53dad1
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-24 13:31:41 +0300

    add modal screen

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,17 +1,24 @@
 <template>
 	<social-links />
 	<note-book />
+	<modal-screen />
 </template>
 
 <script>
 import SocialLinks from "@/components/SocialLinks.vue";
 import NoteBook from "@/components/NoteBook.vue";
+import ModalScreen from "@/components/ModalScreen.vue";
 
 export default {
 	name: "App",
 	components: {
 		SocialLinks,
 		NoteBook,
+		ModalScreen,
+	},
+	// Get task list at component creation
+	created() {
+		this.$store.dispatch("getTaskList");
 	},
 };
 </script>
---
+++ src/components/ModalScreen.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="modal">
+		<div class="modal__overlay"></div>
+		<div class="modal__container">
+			<div class="modal__head">
+				<h2 class="title"><font-icon icon="plus-circle" /> Add new task</h2>
+				<a class="close" title="Close dialog"><font-icon icon="times-circle" /></a>
+			</div>
+			<div class="modal__divider"></div>
+			<p>
+				Eu sint et nisi aute aliqua cillum aliqua veniam cillum. Anim ad officia
+				incididunt sit eu fugiat sit mollit. Cillum deserunt eiusmod occaecat nulla
+				laborum id quis est deserunt incididunt irure irure duis. Consequat
+				exercitation ex do elit enim.
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+import "@/styles/modalscreen.css";
+
+export default {
+	name: "ModalScreen",
+};
+</script>
